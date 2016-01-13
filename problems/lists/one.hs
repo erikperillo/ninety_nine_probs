a0c0e33695abef09f029aa5f4bad97a7f632229f
Erik Perillo <erik.perillo@gmail.com>
2016-01-12 22:36:38 -0200

    problem 1 solved. yahoo!

--- problems/lists/one.hs
+++ problems/lists/one.hs
@@ -2,3 +2,19 @@
 -- Find the last element of a list. 
 
 --solution 1
+myLast :: [a] -> a
+myLast [] = error "empty list"
+myLast [x] = x
+myLast (_:xs) = myLast xs
+
+--solution 2 (obvious one)
+myLast' :: [a] -> a
+myLast' xs = last xs
+
+--solution 3
+myLast'' :: [a] -> a
+myLast'' = head . reverse
+
+--solution 4
+myLast''' :: [a] -> a
+myLast''' xs = xs !! (length xs - 1)
